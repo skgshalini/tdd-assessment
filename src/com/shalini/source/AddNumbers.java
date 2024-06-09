b78a08d37083068ddb8a6c898e73c4bb6b3ed146
Shalini <skgshalinigupta@gmail.com>
2024-06-09 19:47:38 +0530

    first commit

---
+++ src/com/shalini/source/AddNumbers.java
@@ -0,0 +1,15 @@
+package com.shalini.source;
+
+import java.util.ArrayList;
+
+public class AddNumbers {
+
+	public int add(String string) throws Exception {
+		int sum =0;
+		if (string.isEmpty()) {
+            return sum;
+        }
+		return sum;
+     }
+	
+}
---
+++ src/com/shalini/tdd/AddNumbersTest.java
@@ -0,0 +1,19 @@
+package com.shalini.tdd;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.shalini.source.AddNumbers;
+
+public class AddNumbersTest {
+
+	@Test
+	public void testAddNumbers() throws Exception {
+		
+		AddNumbers addNumbers = new AddNumbers();
+		assertEquals(0,addNumbers.add(""));
+		
+	}
+
+}
